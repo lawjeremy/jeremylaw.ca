d647a474dc57191687196baab90fa373c9796752
Jeremy Law <jeremy@jeremylaw.ca>
2019-08-01 19:33:20 -0400

    fix container

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import Img from 'gatsby-image';
